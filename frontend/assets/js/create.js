9d135a38a6a11264e2b48bfa5e7ff320dd324e58
artem <mr.migovan@mail.ru>
2018-04-09 11:55:45 +0500

    fix bug

--- frontend/assets/js/create.js
+++ frontend/assets/js/create.js
@@ -1,12 +1,20 @@
 angular.module('myApp').controller('createCtrl', function ($scope, mainFactory) {
 
-    $scope.flag = true;
     $scope.dataObject = {
         head: "",
         paper: "",
-        img: []
+        img: [21212]
     };
 
+
+
+     $scope.$watch('dataObject.img', () => {
+        console.log($scope.dataObject.img)
+    })
+
+    // $scope.addImage = (event) => {
+    //     console.log(event);
+    // }
 
     $scope.add = () => {
 
@@ -14,47 +22,26 @@ angular.module('myApp').controller('createCtrl', function ($scope, mainFactory) 
             alert("Все поля обязательны к заполнению.");
         } else {
             let articleList = mainFactory.articleList;
+            if (articleList == null) articleList = []
             articleList.push($scope.dataObject);
             mainFactory.articleList = articleList;
 
             let newUrl = "#!/";
             history.pushState('', '', newUrl);
         }
-
-        console.log($scope.dataObject.head)
-        console.log($scope.dataObject.paper)
-        console.log(mainFactory);
     }
 
-    // $scope.$watch('dataObject.img', function (newVal, oldVal) {
-    //     console.log('watch newVal', newVal)
-    //     console.log('watch oldVal', oldVal)
-    // })
+    //  $scope.handleFiles = () => {
+    //     var reader = new FileReader(),
+    //         input = document.getElementById('input').files[0],
+    //         output = document.getElementById('output');
+    //     reader.addEventListener("loadend", function(result) {
+    //         output.innerHTML = result.target.result;
+    //     }, false);
+    //     reader.readAsDataURL(input);
     //
-    // $scope.addImage = (event) => {
-    //     console.log('addImage', event)
     // }
-    //
-    // $scope.addImageTest = (evt) => {
-    //     var files = evt.target.files;
-    //
-    //     for (var i = 0, f; f = files[i]; i++) {
-    //
-    //         var reader = new FileReader();
-    //
-    //         reader.onload = (function(theFile) {
-    //             return function(e) {
-    //                 var span = document.createElement('span');
-    //                 span.innerHTML = ['<img class="thumb" src="', e.target.result,
-    //                     '" title="', escape(theFile.name), '"/>'].join('');
-    //                 document.getElementById('list').insertBefore(span, null);
-    //             };
-    //         })(f);
-    //
-    //         reader.readAsDataURL(f);
-    //     }
-    // }
-    // document.getElementById('files').addEventListener('change',  $scope.addImage, false);
+
 });
 
 
