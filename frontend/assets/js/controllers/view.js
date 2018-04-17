c544a42cda0593be1f5ec49b9288ddbef8918c66
artem <mr.migovan@mail.ru>
2018-04-17 20:24:58 +0500

    добавил слайдер перелистывания статей в режиме просмотра

--- frontend/assets/js/controllers/view.js
+++ frontend/assets/js/controllers/view.js
@@ -2,8 +2,38 @@ angular.module('myApp').controller('viewCtrl', function ($scope, mainFactory, $r
     $scope.data = mainFactory.articleList[$routeParams.Id];
     $scope.Id = $routeParams.Id;
 
-
-
     $scope.tagParagraph = document.getElementsByClassName('paragraph')[0];
     $scope.tagParagraph.style.fontSize = $scope.data.value + "px";
+
+    let n = $routeParams.Id;
+
+    $scope.sliderPlus = () => {
+
+        let newUrl = "#!/view/" + n++;
+        console.log(mainFactory.articleList.length);
+
+        if (mainFactory.articleList.length > n) {
+
+            let newUrl = "#!/view/" + n++;
+            history.pushState('', '', newUrl);
+        }
+        if (mainFactory.articleList.length <= n) {
+            $scope.flagPlus = true;
+        }
+
+        console.log(newUrl);
+    }
+
+    $scope.sliderMinus = () => {
+
+        let newUrl = "#!/view/" + n--;
+
+        if (n >= 0 ) {
+            let newUrl = "#!/view/" + n--;
+            history.pushState('', '', newUrl);
+        } else {
+            $scope.flagMinus = true;
+        }
+    }
+
 })
